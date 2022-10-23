2b1b964c8dfd67436689ee13d626038e131b1fe5
Duke Pham <duke.pmd@gmail.com>
2022-10-23 22:46:49 +0700

    removed unused imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { render } from "@testing-library/react";
-import React, { useEffect } from "react";
+// import { render } from "@testing-library/react";
+import React from "react";
 import { formatTime } from "./formatTime";
 import useTimer from "./useTimer";
 
